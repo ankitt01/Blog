ed1205caa72b522aa3f5657ef6e665c6d9e8888c
ankitt01 <ankitrawat0102@gmail.com>
2021-07-04 16:43:04 +0530

    Displaying the user header

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux' //To wire the action creator
 import {fetchPosts} from '../actions' //action creator
+import UserHeader from './UserHeader';
+
 class PostList extends React.Component {
     componentDidMount() {
       this.props.fetchPosts();
@@ -16,6 +18,7 @@ class PostList extends React.Component {
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
               </div>
+              <UserHeader userId = {post.userId}/>
             </div>
           </div>
         );
