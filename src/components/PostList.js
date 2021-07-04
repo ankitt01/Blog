20b2b7ced50fed39de3818c9f4affb57ffdd29f2
ankitt01 <ankitrawat0102@gmail.com>
2021-07-04 16:23:59 +0530

    rendering list

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux' //To wire the action creator
 import {fetchPosts} from '../actions' //action creator
-class PostList extends Component {
-
+class PostList extends React.Component {
     componentDidMount() {
-        this.props.fetchPosts();
+      this.props.fetchPosts();
     }
-    
-    render() { 
-        console.log(this.props.posts)
-        return <div>PostList</div>
+  
+    renderList() {
+      return this.props.posts.map(post => {
+        return (
+          <div className="item" key={post.id}>
+            <i className="large middle aligned icon user" />
+            <div className="content">
+              <div className="description">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          </div>
+        );
+      });
     }
-}
- 
-const mapStateToProps = (state) => {
-    return {posts: state.posts};
-}
+  
+    render() {
+      return <div className="ui relaxed divided list">{this.renderList()}</div>;
+    }
+  }
+  
+  const mapStateToProps = state => {
+    return { posts: state.posts };
+  };
 export default connect(mapStateToProps, {fetchPosts})(PostList);
 
 //whenever we have to tranfer data from redux to react side
